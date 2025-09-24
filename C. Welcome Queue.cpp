f8b3054ba129e5ba40b0f8e84b541ea72d191d3a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-24 15:10:08 +0300

    STLs Problems Solved By c++

---
+++ C. Welcome Queue.cpp
@@ -0,0 +1,40 @@
+// C. Welcome Queue
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+queue<ll>qu;
+void solve()
+{
+    ll t ; cin >> t ;
+    while(t--)
+    {
+        ll id , n ; cin >> id >> n ;
+        if(id ==1 )
+        {
+            qu.push(n) ;
+        }
+        else  // id == 2
+        {
+            if(qu.empty())
+            {
+                cout<<"no"<< endl;
+            }
+            else
+            {
+                if(qu.front() == n)
+                {
+                    cout<<"yes"<< endl;
+                }
+                else
+                {
+                    cout<<"no"<< endl;
+                }
+                qu.pop() ;
+            }
+        }
+    }
+}
+int main()
+{
+    solve();
+}
