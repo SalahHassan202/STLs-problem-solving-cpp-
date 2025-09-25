d8bb009837b552cd0f2c673284d7731fc146e111
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-25 22:00:24 +0300

    STLs Problems Solved By c++

---
+++ C. Stack.cpp
@@ -0,0 +1,38 @@
+// C. Stack
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    string s ; cin >> s ;
+    stack<char> res ;
+    ll ans = 0 ;
+   for(int i=0 ; i<s.size() ; i++)
+   {
+    
+        if(s[i] == ')')
+        {
+            if(!res.empty())
+            {
+                res.pop() ;
+                ans+=2 ;
+            } 
+        }
+        else
+        {
+            res.push(s[i]);
+        }
+   }
+
+   cout << ans << endl;
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    solve();
+    
+    return 0 ;
+}
