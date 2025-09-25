0a41b57c8a26f0346a1ac85b0cb00b56e0bd7f1c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-09-25 03:27:49 +0300

    STLs Problems Solved By c++

---
+++ G. Throwing cards away.cpp
@@ -0,0 +1,36 @@
+// G. Throwing cards away
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std ;
+void solve()
+{
+    int n ; cin >>n  ;
+    queue<ll> q ;
+    for(int i = 1 ; i <= n ; i++)
+    {
+        q.push(i) ;
+
+    }
+    while(q.size() != 1)
+    {
+        // 1 2 3 4 5 6 7   ===> cout 1
+        // 2 3 4 5 6 7     ===> pop(2) 
+        // 3 4 5 6 7 2     ===> push(2)
+        cout<<q.front() << " " ;
+        q.pop() ;
+        q.push(q.front()) ;
+        q.pop() ;
+    }
+    cout << endl; 
+    cout << q.front() << endl;
+}
+int main()
+{
+    // fast code
+    ios::sync_with_stdio(false),cout.tie(NULL),cin.tie(0);
+    
+    solve();
+    
+    return 0 ;
+}
