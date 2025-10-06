1286719908b61ab252d4c95697bd5c53259346c1
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-10-06 23:49:44 +0300

    STLs Problems Solved By c++

---
+++ U. Game with string.cpp
@@ -0,0 +1,42 @@
+// U. Game with string
+
+#include<bits/stdc++.h>
+#define ll long long
+#define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std ;
+
+void solve()
+{
+    string s ; cin >> s;
+    stack<char>st;
+    ll cnt = 0;
+    for(int i = 0 ; i<s.size() ; i++)
+    {
+        if(!st.empty() && st.top() == s[i])
+        {
+            st.pop() ; 
+            cnt++;
+        }
+        else
+        {
+            st.push(s[i]) ;
+        }
+    }
+
+   cout <<  (cnt%2 !=0 ? "Yes" : "No") << endl ; 
+   
+}
+int main()
+{
+    fast;
+    
+    ll t = 1 ;
+    // cin >> t ;
+    while(t--)
+    {
+        solve();
+    }
+  
+    
+    return 0 ;
+}
