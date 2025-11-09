cd0f19172be4bb332d392404e945696e7c6550a7
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-09 22:02:24 +0200

    Update P. Permutation Minimization by Deque.cpp

--- P. Permutation Minimization by Deque.cpp
+++ P. Permutation Minimization by Deque.cpp
@@ -7,9 +7,9 @@ using namespace std ;
 
 void solve()
 {
-    ll n ; cin >> n ;
+    ll n ;       cin >> n ;
     vector<ll>p(n);
-    for(int i=0;i<n ;i++) cin>>p[i] ;
+    for(int i=0;i<n ;i++)     cin>>p[i] ;
     deque<ll>dq;
     for(int i=0 ;i<n ;i++)
     {
