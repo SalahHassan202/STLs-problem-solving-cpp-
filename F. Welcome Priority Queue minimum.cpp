250c5b5d6cab817cb3a33270447377edaf215436
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-09 21:57:18 +0200

    Update F. Welcome Priority Queue minimum.cpp

--- F. Welcome Priority Queue minimum.cpp
+++ F. Welcome Priority Queue minimum.cpp
@@ -6,7 +6,7 @@ using namespace std ;
 priority_queue<ll , vector<ll> , greater<ll>>pq;    // smallestnumber
 void solve()
 {
-    ll t ; cin >> t ;
+    ll t ;     cin >> t ;
     while(t--)
     {
         string s ; cin >> s ;
@@ -15,18 +15,18 @@ void solve()
             ll n ; cin >> n ;
             pq.push(n) ;
 
-        }else if (s == "pop")
+        }
+        else if (s == "pop")
         {
             if(!pq.empty())
-            {
                 pq.pop() ;
-            }
-        }else //(s == "top")
+           
+        }
+        else    //(s == "top")
         {
             if(!pq.empty())
-            {
-                cout << pq.top() << endl;
-            }
+               cout << pq.top() << endl;
+           
         }
     }
 }
