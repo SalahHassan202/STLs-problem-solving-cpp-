6610766cb149de87eca29de15bb65a12ccb9706d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-09 22:01:48 +0200

    Update O. Prepend and Append.cpp

--- O. Prepend and Append.cpp
+++ O. Prepend and Append.cpp
@@ -5,11 +5,10 @@
 #define fast ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 using namespace std ;
 
-
 void solve()
 {
-    ll n ; cin >> n ;
-    string s ; cin >> s ;
+    ll n ;     cin >> n ;
+    string s ;      cin >> s ;
     deque<char>dq;
     for(int i=0;i<n;i++)
     {
