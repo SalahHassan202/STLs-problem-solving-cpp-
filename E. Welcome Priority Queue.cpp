4fe593d2cdb91a59089a29c8891e08b00a4f2b4c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-09 21:56:33 +0200

    Update E. Welcome Priority Queue.cpp

--- E. Welcome Priority Queue.cpp
+++ E. Welcome Priority Queue.cpp
@@ -1,31 +1,30 @@
 // E. Welcome Priority Queue
+
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std ;
 priority_queue<ll>pq;
 void solve()
 {
-    ll t ; cin >> t ;
+    ll t ;     cin >> t ;
     while(t--)
     {
-        string s ; cin >> s ;
+        string s ;     cin >> s ;
         if(s == "push")
         {
-            ll n ; cin >> n ;
+            ll n ;     cin >> n ;
             pq.push(n) ;
 
-        }else if (s == "pop")
+        }
+        else if (s == "pop")
         {
             if(!pq.empty())
-            {
-                pq.pop() ;
-            }
-        }else //(s == "top")
+               pq.pop() ;
+        }
+        else    //(s == "top")
         {
             if(!pq.empty())
-            {
-                cout << pq.top() << endl;
-            }
+              cout << pq.top() << endl; 
         }
     }
 }
