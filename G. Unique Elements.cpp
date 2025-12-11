4178aa3c0ffc7914fe21e5ddcb56c8e34de21de6
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-11 23:14:00 +0200

    Added G. Unique Elements Probelm

---
+++ G. Unique Elements.cpp
@@ -0,0 +1,44 @@
+// G. Unique Elements
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    map<ll, ll> mp;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        mp[v[i]]++;
+    }
+    ll res = 0;
+    for (auto i : mp)
+    {
+        if (i.second == 1)
+        {
+            res++;
+        }
+    }
+
+    cout << res;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
